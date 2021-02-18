71f020c083d2cad0c4ca197c3f7215763e179b3e
Calvin Jacobsen <Jaco1932@umn.edu>
2021-02-17 22:34:48 -0600

    completed writing writeto system with inquirer prompts

--- index.js
+++ index.js
@@ -2,74 +2,81 @@
 const fs = require('fs');
 var inquirer = require('inquirer');
 
-fs.readFile('message.txt', 'utf8', function (error, data) {
-    if (error) {
-        console.error(error)
-    }
-    else {
-        console.log(data)
-    }
-});
+//function to write README file
+function writeToFile(filename, data) {
 
-// TODO: Create an array of questions for user input
-inquirer.prompt([
-    {
-        type: 'input',
-        name: 'projectTitle',
-        message: 'Enter a name in for your project: '
-    },
-    {
-        type:'input',
-        name: 'description',
-        message: 'Description: '
-    },
-    {
-        type:'input',
-        name: 'installation',
-        message: 'Installation process : '
-    },
-    {
-        type:'input',
-        name: 'usage',
-        message: 'Usage: '
-    },
-    {
-        type: 'input',
-        name: 'contributions',
-        message: 'Author: ',
-    },
-    {
-        type: 'input',
-        name: 'license',
-        message: 'Lisense: ' (MIT)
-        
-    },
-    {
-        type: 'input',
-        name: 'contributions',
-        message: 'Contributors: ',
-    },
-    {
-        type: 'input',
-        name: 'tests',
-        message: 'Tests: ',
+    fs.writeFile(filename, data, (err) => {
+        // In case of a error throw err. 
+        if (err) throw err;
+    });
 
-    }
-    
-
-
-]).then((resoponsesObj) => {
-
-let fileBody = 
-
-    
-}
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) { }
+};
 
 // TODO: Create a function to initialize app
-function init() { }
+function init() {
+    //An array of questions for user input
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'projectTitle',
+            message: 'Enter a name in for your project: '
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Description: '
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Installation process : '
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Usage: '
+        },
+        {
+            type: 'input',
+            name: 'contributions',
+            message: 'Author: ',
+        },
+        {
+            type: 'input',
+            name: 'license',
+            message: 'Lisense: '
+
+        },
+        {
+            type: 'input',
+            name: 'contributions',
+            message: 'Contributors: ',
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'Tests: ',
+
+        }
+
+
+    ]).then((resoponsesObj) => {
+        //inputing responses to file string --> then inputting into md file
+        const filename = `${resoponsesObj.projectTitle.toLowerCase().split(' ').join('')}.md`;
+        //initializing and putting Project title at top
+        let fileBody = `# ${resoponsesObj.projectTitle} \n\n`;
+
+        //inputs description
+        fileBody += `## Description \n\n ${resoponsesObj.description} \n\n`;
+        fileBody += `## Installation Instructions \n\n ${resoponsesObj.installation} \n\n`;
+        fileBody += `## Usage Information \n\n ${resoponsesObj.usage} \n\n`;
+        fileBody += `## Contributions \n\n ${resoponsesObj.contributions} \n\n`;
+        fileBody += `## Tests \n\n ${resoponsesObj.tests} \n\n`;
+
+        writeToFile(filename, fileBody);
+    });
+
+};
 
 // Function call to initialize app
 init();
